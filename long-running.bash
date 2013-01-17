8166f32c08d624353218a450733a0d23a38f1e3d
Michael Neuling <mikey@neuling.org>
2013-01-18 10:35:33 +1100

    Use env vars rather than the cache dir
    Make code simpler.

--- long-running.bash
+++ long-running.bash
@@ -27,33 +27,6 @@ fi
 
 function notify_when_long_running_commands_finish_install() {
 
-    # A directory containing files for each currently running shell (not
-    # subshell), each named for their PID.  Each file is either empty,
-    # indicating that no command is running, or contains information about the
-    # currently running command for that shell.
-    local running_commands_dir=~/.cache/running-commands
-
-    mkdir -p $running_commands_dir
-
-    # Clear out any old PID files.  That is, any files named after a PID
-    # that's not currently running bash.
-    for pid_file in $running_commands_dir/*; do
-        local pid=$(basename $pid_file)
-        # If $pid is numeric, then check for a running bash process.
-        case $pid in
-        ''|*[!0-9]*) local numeric=0 ;;
-        *) local numeric=1 ;;
-        esac
-
-        if [[ $numeric -eq 1 ]]; then
-            local command=$(ps --no-headers -o command $pid)
-            if [[ $command != $BASH ]]; then
-                rm -f $pid_file
-            fi
-        fi
-    done
-    unset pid_file
-
     function active_window_id () {
         if [[ -n $DISPLAY ]] ; then
             set - $(xprop -root _NET_ACTIVE_WINDOW)
@@ -63,57 +36,39 @@ function notify_when_long_running_commands_finish_install() {
         echo nowindowid
     }
 
-    # The file containing information about the currently running command for
-    # this shell.  Either empty (meaning no command is running) or in the
-    # format "$start_time\n$command", where $command is the currently running
-    # command and $start_time is when it started (in UNIX epoch format, UTC).
-    last_command_started_cache=$running_commands_dir/$$
-
     function precmd () {
 
-        if [[ -r $last_command_started_cache ]]; then
+        if [[ -n "$last_command_started" ]]; then
+            local now current_window
 
-            local last_command_started last_command last_window
-
-            {
-                read last_command_started
-                read last_command
-                read last_window
-            } < $last_command_started_cache
-            if [[ -n "$last_command_started" ]]; then
-                local now current_window
-
-                printf -v now "%(%s)T" -1
-                current_window=$(active_window_id)
-                if [[ $current_window != $last_window ]] ||
-                   [[ $current_window == "nowindowid" ]] ; then
-                    local time_taken=$(( $now - $last_command_started ))
-                    if [[ $time_taken -gt $LONG_RUNNING_COMMAND_TIMEOUT ]] &&
-                       [[ -n $DISPLAY ]] ; then
-                        notify-send \
-                          -i utilities-terminal \
-                          -u low \
-                          "Long command completed" \
-                          "\"$last_command\" took $time_taken seconds"
-                    fi
-                    if [[ -n $LONG_RUNNING_COMMAND_CUSTOM_TIMEOUT ]] &&
-                       [[ -n $LONG_RUNNING_COMMAND_CUSTOM ]] &&
-                       [[ $time_taken -gt $LONG_RUNNING_COMMAND_CUSTOM_TIMEOUT ]] ; then
-                        # put in brackets to make it quiet
-                        ( $LONG_RUNNING_COMMAND_CUSTOM \
-                            "\"$last_command\" took $time_taken seconds" & )
-                    fi
+            printf -v now "%(%s)T" -1
+            current_window=$(active_window_id)
+            if [[ $current_window != $last_window ]] ||
+                [[ $current_window == "nowindowid" ]] ; then
+                local time_taken=$(( $now - $last_command_started ))
+                if [[ $time_taken -gt $LONG_RUNNING_COMMAND_TIMEOUT ]] &&
+                    [[ -n $DISPLAY ]] ; then
+                    notify-send \
+                        -i utilities-terminal \
+                        -u low \
+                        "Long command completed" \
+                        "\"$last_command\" took $time_taken seconds"
+                fi
+                if [[ -n $LONG_RUNNING_COMMAND_CUSTOM_TIMEOUT ]] &&
+                    [[ -n $LONG_RUNNING_COMMAND_CUSTOM ]] &&
+                    [[ $time_taken -gt $LONG_RUNNING_COMMAND_CUSTOM_TIMEOUT ]] ; then
+                    # put in brackets to make it quiet
+                    ( $LONG_RUNNING_COMMAND_CUSTOM \
+                        "\"$last_command\" took $time_taken seconds" & )
                 fi
             fi
-            # No command is running, so clear the cache.
-            echo -n > $last_command_started_cache
         fi
     }
 
     function preexec () {
-        printf "%(%s)T\n" -1 > $last_command_started_cache
-        echo "$1" >> $last_command_started_cache
-        active_window_id >> $last_command_started_cache
+        last_command_started=$(printf "%(%s)T\n" -1)
+        last_command=$(echo "$1")
+        last_window=$(active_window_id)
     }
 
     preexec_install
